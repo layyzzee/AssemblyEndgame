e23e18752920803189c6e0571501c53e287a30b9
Kati Stark <kati.stark95@gmail.com>
2026-08-17 12:48:12 +0100

    Add extra a11y functionaility to say whether a guess was correct

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,8 @@ export default function AssemblyEndgame() {
   const isGameWon = currentWord.split("").every(letter => guessedLetters.includes(letter))
   const isGameLost = wrongGuessCount >= languages.length - 1
   const isGameOver = isGameWon || isGameLost
+  const lastGuessedLetter = guessedLetters[guessedLetters.length - 1]
+  const numGuessesLeft = languages.length - 1
 
   return (
     <main>
@@ -47,6 +49,13 @@ export default function AssemblyEndgame() {
         aria-live="polite"
         role="status"
       >
+        <p>
+          {currentWord.includes(lastGuessedLetter) ?
+            `Correct! The letter ${lastGuessedLetter} is in the word.` :
+            `Sorry, the letter ${lastGuessedLetter} is not in the word.`
+          }
+          You have {numGuessesLeft} attempts left.
+        </p>
         <p>Current word: {currentWord.split("").map(letter =>
           guessedLetters.includes(letter) ? letter + "." : "blank.")
           .join(" ")}</p>
